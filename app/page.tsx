af04ec1e53c6ccca5ec093a1cf4aa6789938f889
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-11-01 10:41:07 +0100

    changes in nav + new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import GameHeroText from './components/GameHeroText/GameHeroText';
-import RegisterEmailForm from './components/RegisterEmailForm/RegisterEmailForm';
+import EcoWork from "./components/EcoWork/EcoWork";
+import GameHeroText from "./components/GameHeroText/GameHeroText";
+import RegisterEmailForm from "./components/RegisterEmailForm/RegisterEmailForm";
 
 export default function Home() {
   return (
-    <div className='home'>
-      <GameHeroText />
-      <div>
+    <div className="home">
+      <div className="home__game-form">
+        <GameHeroText />
         <RegisterEmailForm />
+      </div>
+      <div className="home__eco">
+        <EcoWork />
       </div>
     </div>
   );
