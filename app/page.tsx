59a23ec496cf18ad0591b299c7d73e4e4a689d2c
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-11-01 15:13:38 +0100

    hero changes ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
-import Actions from './components/Actions/Actions';
-import EcoWork from './components/EcoWork/EcoWork';
-import GameHeroText from './components/GameHeroText/GameHeroText';
-import RegisterEmailForm from './components/RegisterEmailForm/RegisterEmailForm';
-import Sponsors from './components/Sponsors/Sponsors';
+import Actions from "./components/Actions/Actions";
+import EcoWork from "./components/EcoWork/EcoWork";
+import GameHeroText from "./components/GameHeroText/GameHeroText";
+import RegisterEmailForm from "./components/RegisterEmailForm/RegisterEmailForm";
+import Sponsors from "./components/Sponsors/Sponsors";
+import Image from "next/image";
+import heroImg from "./images/hero-img.svg";
 
 export default function Home() {
   return (
-    <div className='home'>
-      <div className='home__eco-form'>
+    <div className="home">
+      <Image className="home__hero" src={heroImg} alt="" />
+
+      <div className="home__eco-form">
         <GameHeroText />
         <RegisterEmailForm />
         <Actions />
       </div>
 
-      <div className='home__eco-work'>
+      <div className="home__eco-work">
         <EcoWork />
       </div>
       <Sponsors />
